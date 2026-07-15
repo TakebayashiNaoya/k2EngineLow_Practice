4fcb2caa9c71f470e323b22ee379a12c7f66c4ed
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2026-07-15 13:37:27 +0900

    Step 1-4 : `ModelRender` を自作する

---
+++ Game/ModelRender.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include "ModelRender.h"
+
+
+void ModelRender::Init(const char* modelFilePath)
+{
+	ModelInitData modelInitData;
+	modelInitData.m_tkmFilePath = modelFilePath;
+	modelInitData.m_fxFilePath = "Assets/shader/model.fx";
+	model_.Init(modelInitData);
+}
+
+
+void ModelRender::Update()
+{
+	model_.UpdateWorldMatrix(position_, rotation_, scale_);
+}
+
+
+void ModelRender::Draw(RenderContext & renderContext)
+{
+	model_.Draw(renderContext);
+}

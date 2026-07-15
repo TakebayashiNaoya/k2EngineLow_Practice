5b56c1085f5fa68762695165a46e5f3ea294668e
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2026-07-15 12:15:10 +0900

    Step 1-2 : `SpriteRender` を自作する (#3)

---
+++ Game/SpriteRender.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+
+class SpriteRender
+{
+public:
+	/** 座標を設定 */
+	inline void SetPosition(const Vector3& position) { position_.Set(position.x, position.y); }
+	inline void SetPosition(const Vector2& position) { position_ = position; }
+	inline void SetPosition(float x, float y) { position_.Set(x, y); }
+	/** 拡縮を設定 */
+	inline void SetScale(const Vector3& scale) { scale_.Set(scale.x, scale.y); }
+	inline void SetScale(const Vector2& scale) { scale_ = scale; }
+	inline void SetScale(float x, float y) { scale_.Set(x, y); }
+	/** 回転を設定 */
+	inline void SetRotation(const Quaternion& rotation) { rotation_ = rotation; }
+
+
+public:
+	/** 初期化 */
+	void Init(const char* ddsFilePath,float width, float height);
+	/** 更新 */
+	void Update();
+	/** 描画 */
+	void Draw(RenderContext& renderContext);
+
+	SpriteRender();
+	~SpriteRender();
+
+
+private:
+	Vector2	   position_;  /** 座標 */
+	Vector2	   scale_;     /** 拡縮 */
+	Quaternion rotation_;  /** 回転 */
+
+	Sprite	   sprite_;	   /** スプライト */
+};
+
